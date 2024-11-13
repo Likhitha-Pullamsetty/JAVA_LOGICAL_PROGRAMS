ebf3c164fbd43fe54accab44b6035874a7fb33f8
Likhitha-Pullamsetty <143479698+Likhitha-Pullamsetty@users.noreply.github.com>
2024-11-13 02:02:54 -0800

    Add files via upload

---
+++ Day1/Employee.java
@@ -0,0 +1,12 @@
+class Employee 
+{
+  public static void main(String args[])
+  {
+    int num=37;
+    String name="Likhitha";
+    float salary=54434.45F;
+    System.out.println("The Employee number is:"+num);
+    System.out.println("The Employee name is:"+name);
+    System.out.println("The Employee salary is:"+salary);
+  }
+}
---
+++ Day1/Student.java
@@ -0,0 +1,14 @@
+class Student
+{
+   public static void main(String args[])
+   {
+      int stuno=37;
+      String stuname="Likhitha";
+      String stucou="MCA";
+      String stuadd="Vijayawada";
+      System.out.println("The Student No is:"+stuno);
+      System.out.println("The Student Name is:"+stuname);
+      System.out.println("The Student Course is:"+stucou);
+      System.out.println("The Student Address is:"+stuadd);
+    }
+}
